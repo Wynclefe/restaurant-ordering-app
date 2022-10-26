eeaf2cde9eaa8d2a6b6e0afa94ca245f2c4b5e0a
wynclefe <wynclefe@gmail.com>
2022-10-26 13:45:45 +0800

    Add menu section

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 import {menuArray} from './data.js'
 
 const menuSection = document.getElementById('menu-section')
+
+function getMenuHtml() {
+    let menuHtml = ``
+
+    menuArray.forEach(function(menu){
+        menuHtml += `
+            <div class="menu-section">
+                <div class="menu-text">
+                    <p class="product-name">${menu.name}</p>
+                    <p class="ingredients">${menu.ingredients}</p>
+                    <p class="price">${menu.price}</p>
+                    <i class="fa-solid fa-plus" data-id="${menu.id}"></i>
+                </div>
+                <div class="emoji-image>${menu.emoji}</div>
+            </div>
+        `
+    })
+}
