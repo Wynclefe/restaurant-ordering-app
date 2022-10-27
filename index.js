3641e92fb35a06849b0bc964b9061894144067aa
wynclefe <wynclefe@gmail.com>
2022-10-27 13:10:58 +0800

    Switch places

--- index.js
+++ index.js
@@ -8,21 +8,20 @@ function getMenuHtml() {
     menuArray.forEach(function(menu){
         menuHtml += `
             <div class="menu-section">
-            <div class="emoji-image">${menu.emoji}</div>
+                <p class="emoji-image">${menu.emoji}</p>
+            <div 
                 <div class="menu-text">
                     <p class="product-name">${menu.name}</p>
                     <p class="ingredients">${menu.ingredients}</p>
                     <p class="price">${menu.price}</p>
-                    <i class="fa-solid fa-plus" data-id="${menu.id}"></i>
                 </div>
+                <i class="fa-solid fa-plus plus-icon" data-id="${menu.id}"></i>
             </div>
         `
     })
 
     return menuHtml
 }
-
-console.log(getMenuHtml())
 
 function render() {
  menuSection.innerHTML = getMenuHtml()
