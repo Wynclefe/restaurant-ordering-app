787260a156d8c13a640358ebd266258dae0ed4e7
wynclefe <wynclefe@gmail.com>
2022-10-27 12:00:58 +0800

    Switch places for menu text and emoji image

--- index.js
+++ index.js
@@ -8,13 +8,13 @@ function getMenuHtml() {
     menuArray.forEach(function(menu){
         menuHtml += `
             <div class="menu-section">
+            <div class="emoji-image">${menu.emoji}</div>
                 <div class="menu-text">
                     <p class="product-name">${menu.name}</p>
                     <p class="ingredients">${menu.ingredients}</p>
                     <p class="price">${menu.price}</p>
                     <i class="fa-solid fa-plus" data-id="${menu.id}"></i>
                 </div>
-                <div class="emoji-image">${menu.emoji}</div>
             </div>
         `
     })
